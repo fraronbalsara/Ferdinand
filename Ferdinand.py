fa6f4669ddcedec8b757b14f17fc0fe89907154b
fraronbalsara <32577463+fraronbalsara@users.noreply.github.com>
2019-07-06 20:33:28 +0530

    Update Ferdinand.py

--- Ferdinand.py
+++ Ferdinand.py
@@ -18,7 +18,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="on %d servers"%(a)))
     print('Bot is online and ready to use.')
 
-extensions=['miscellaneous','score','currency','giveaway','clashroyale','translate','casino']
+extensions=['miscellaneous']
 if __name__ == '__main__':
     for extension in extensions:
         try:
