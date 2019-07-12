ffad72c0b3906baf55422d73e71015e5b841c8d0
fraronbalsara <32577463+fraronbalsara@users.noreply.github.com>
2019-07-12 22:58:19 +0530

    Update miscellaneous.py

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -28,18 +28,6 @@ class miscellaneous(commands.Cog):
             await ctx.send('Tails')
 
     @commands.command()
-    async def help(self, ctx):
-        author = ctx.message.author.id
-        embed_help = discord.Embed(
-            title="```Help- Commands list for Ferdinand.```",
-            colour=discord.Colour.green()
-        )
-        embed_help.add_field(name='.ping', value='Returns Pong :ping_pong:', inline=True)
-        embed_help.add_field(name='.toss', value='Returns the value of a random coin toss. Either __heads__ or __tails__', inline=True)
-        embed_help.add_field(name='.score', value='Returns the current score of the user in the server.', inline=True)
-        await ctx.send(embed = embed_help)
-
-    @commands.command()
     async def crystal_ball(self, ctx, *que):
         if(que[len(que) - 1].endswith("?")):
             a = ctx.message.author.id
@@ -96,6 +84,55 @@ class miscellaneous(commands.Cog):
         elif(roll2 == 6):
             mes = mes + "<:6_:593094183527645204>"
         await ctx.send(mes)
+        
+    @commands.command()
+    async def help(self, ctx):
+        author = ctx.message.author
+        embed_help = discord.Embed(
+            title="```Help- Commands list for Ferdinand.```",
+            colour=discord.Colour.blue()
+        )
+        embed_help.add_field(name='.ping', value='Returns Pong :ping_pong:', inline=True)
+        embed_help.add_field(name='.toss', value='Returns the value of a random coin toss. Either __heads__ or __tails__', inline=True)
+        embed_help.add_field(name='.dice', value='Returns the value of a random dice roll.', inline=True)
+        embed_help.add_field(name='.twoDice', value='Returns the value of two random dice rolls.', inline=True)
+        embed_help.add_field(name='.crytsal_ball <question>', value='Returns an answer to the question.', inline=True)
+        embed_help.add_field(name='.translate <text>', value='Returns given text translated in English.', inline=True)
+        embed_help.add_field(name='.translateTo <ISO 639-1 language code> <text>', value='Returns given text translated in the language of mentioned ISO 639-1 code.', inline=True)
+        embed_help.add_field(name='.score <tag discord user>(optional)', value='Returns the current score of the user in the server or the current score of the mentioned user in the server.', inline=True)
+        embed_help.add_field(name ='.create_account', value='Creates an acount and assigns you a daily bonus.', inline=True)
+        embed_help.add_field(name ='.daily', value='To collect your daily bonus. Available once every 24 hours', inline=True)
+        embed_help.add_field(name ='.balance <tag discord user>(optional)', value='To check your account balance or the account balance of mentioned user', inline=True)
+        embed_help.add_field(name=".transfer <tag discord user> <amount>", value="Transfers given cash from your account to mention user's account.", inline=True)
+        embed_help.add_field(name='.giveaway_help', value='Help for giveaway related commands. These commands can only be used by those with manage roles permission in the server.', inline=True)
+        embed_help.add_field(name='.moderation_help', value='Help for moderation related comamnds. This command can only be used by those with manage roles permission in the server.', inline=True)
+        await author.send(embed = embed_help)
+
+    @commands.command()
+    @commands.has_permissions(manage_roles = True)
+    async def giveaway_help(self, ctx):
+        author = ctx.message.author
+        embed_help = discord.Embed(
+            title="```Giveaway Help- Commands list for Ferdinand.```",
+            colour=discord.Colour.blue()
+        )
+        embed_help.add_field(name='.regular_giveaway_start <time: in minutes>', value='Starts a regular giveaway where users react to bot post and a random winner is chosen after given time.', inline=True)
+        embed_help.add_field(name='.qa_giveaway_start <time: in minutes> <answer> <question>', value="Starts a question-answer giveaway where the bot allows users to join the giveaway session until mentioned time, following which it opens the channel to the registered users and let's them answer the question. Bot automatically reads all answers and picks the correct one and announces the winner.", inline=True)
+        await author.send(embed = embed_help)
+
+    @commands.command()
+    @commands.has_permissions(manage_roles = True)
+    async def moderation_help(self, ctx):
+        author = ctx.message.author
+        embed_help = discord.Embed(
+            title="```Moderation Help- Commands list for Ferdinand.```",
+            colour=discord.Colour.blue()
+        )
+        embed_help.add_field(name='.kick <tag discord user> <reason>(optional)', value='To kick a user with optional argument reason.', inline=True)
+        embed_help.add_field(name='.ban <tag discord user> <reason>(optional)', value='To bam a user with optional argument reason.', inline=True)
+        embed_help.add_field(name='.mute <tag discord user> <time>(optional) <reason>(optional)', value='To mute a user with optional argument reason and specified time.', inline=True)
+        embed_help.add_field(name='.unmute <tag discord user> <reason>(optional)', value='To unute a user.', inline=True)
+        await author.send(embed = embed_help)
 
 def setup(client):
     client.add_cog(miscellaneous(client))
