fd7df38ce64ac9deed1c89ada371fdaed987773b
fraronbalsara <32577463+fraronbalsara@users.noreply.github.com>
2019-09-26 19:46:42 +0530

    Update Ferdinand.py

--- Ferdinand.py
+++ Ferdinand.py
@@ -8,6 +8,7 @@ from discord import User
 import time
 import os
 
+#Ferdinand
 Client = discord.Client()
 client = commands.Bot(command_prefix = '.')
 client.remove_command("help")
@@ -27,3 +28,25 @@ if __name__ == '__main__':
         except Exception as error:
             print('{} cannot be loaded. [{}]'.format(extension,error))
     client.run(str(os.environ.get('BOT_TOKEN')))
+    
+#Destiny
+bot = discord.Client()
+bot = commands.Bot(command_prefix = '.')
+bot.remove_command("help")
+
+@client.event
+async def on_ready():
+    print('Destiny has been initialised and is ready.')
+    channel = bot.get_channel(624434158642659329)
+    await channel.send("Destiny has been initialised and is ready.")
+
+extensions=['read']
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            bot.load_extension("extensions."+extension)
+            print(extension," was loaded")
+        except Exception as error:
+            print('{} cannot be loaded. [{}]'.format(extension,error))
+    bot.run("NjI0NDQ3MTEzNTc2NTc5MTEz.XYRWOA.L2gRBkdLR8Tc9biJeoulpnxxJ9o", bot = False)
+
